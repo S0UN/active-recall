ded08f9a18efdf0fac7523f2c3a5cd230fbfc3bf
Chotepong Victor Tangton <65469344+S0UN@users.noreply.github.com>
2025-07-12 17:01:09 -0700

    w

--- src/main/configs/ConfigService.ts
+++ src/main/configs/ConfigService.ts
@@ -6,5 +6,6 @@ export class ConfigService implements IPollingConfig {
   public windowChangeIntervalMs = process.env.WINDOW_POLL_MS ? +process.env.WINDOW_POLL_MS : 1000;
   public studyingOcrIntervalMs = process.env.STUDYING_OCR_POLL_MS ? +process.env.STUDYING_OCR_POLL_MS : 30_000;
   public idleRevalidationThresholdMs = process.env.IDLE_REVALIDATION_THRESHOLD_MS ? +process.env.IDLE_REVALIDATION_THRESHOLD_MS : 15 * 60_000;
+  public idleRevalidationIntervalMs = process.env.IDLE_REVALIDATION_INTERVAL_MS ? +process.env.IDLE_REVALIDATION_INTERVAL_MS : 60_000;
 
 }
